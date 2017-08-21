46450545d182dc8eb3eea3f607ee502fd1cdd861
Stan Smoltis <smoltis@yandex.ru>
2017-08-21 13:27:06 +1000

    bugfix

--- MapReduceReadFile/SalesStats.cs
+++ MapReduceReadFile/SalesStats.cs
@@ -6,7 +6,7 @@ namespace MapReduceReadFile
     public class SalesStats
     {
         public Dictionary<int,string> Stores { get; set; } = new Dictionary<int, string>();
-        public Dictionary<int, int> DistinctBasketsPerStore { get; set; } = new Dictionary<int, int>();
+        public HashSet<Tuple<int, int, int, DateTime>> DistinctBasketsPerStore { get; set; } = new HashSet<Tuple<int, int, int, DateTime>>();
         public Dictionary<int, double> SalesPerStore { get; set; } = new Dictionary<int, double>();
     }
 }
